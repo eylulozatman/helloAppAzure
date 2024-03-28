bce12a4e9c4434b9dc43625dbaa3d5bb2f995040
eylulozatman <eylulozatman@gmail.com>
2024-03-28 17:11:48 +0300

    hello endpoint created

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/func1', (req, res) => {
     res.send('my initial function');
 });
 
+app.get('/hello', (req, res) => {
+    res.send('hellooo');
+});
+
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
 });
