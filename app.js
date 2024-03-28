c09874d5019004840c5f357e72730cf236d21a31
eylulozatman <eylulozatman@gmail.com>
2024-03-28 17:08:12 +0300

    npm libraries and func1

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+
+const app = express();
+const port = 3000; 
+
+app.get('/func1', (req, res) => {
+    res.send('my initial function');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
