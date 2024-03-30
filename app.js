8d4a535fc40603371d504b8744380e380de01655
eylulozatman <eylulozatman@gmail.com>
2024-03-30 20:05:45 +0300

    pg changes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 const express = require('express');
+const config = require('./config');
+const { Pool } = require('pg');
 
-//DEPLOYMENT
+
 const app = express();
 const port = 3000; 
 const PORT = process.env.PORT || 3000;
+
+//DEPLOYMENT
 
 app.get('/', (req, res) => {
     res.send('my initial function, Hello');
@@ -37,35 +41,34 @@ app.get('/f1', (req, res) => {
 
 //f2 endpoint deleted
 
-const config = require('./config');
-const { Pool } = require('pg');
-
 async function connectToPostgres() {
     try {
-        const username = await config.getSecret("postgres-username-secret-name");
-        const password = await config.getSecret("postgres-password-secret-name");
-        const url = await config.getSecret("postgres-url-secret-name");
+        // Key Vault'tan gizli bilgileri al
+        const url = await config.getSecret("url");
+        const username = await config.getSecret("username");
+        const password = await config.getSecret("password");
 
+        // PostgreSQL bağlantı dizesini oluştur
         const connectionString = `postgres://${username}:${password}@${url}`;
 
+        // PostgreSQL havuzu oluştur
         const pool = new Pool({
             connectionString: connectionString
         });
 
-        pool.query('SELECT NOW()', (err, res) => {
-            if (err) {
-                console.error('Error connecting to PostgreSQL:', err);
-            } else {
-                console.log('Connected to PostgreSQL:', res.rows[0].now);
-            }
-            pool.end();
-        });
+        // PostgreSQL'e bağlan ve sorguyu gerçekleştir
+        const client = await pool.connect();
+        const result = await client.query('SELECT NOW()');
+        console.log('Connected to PostgreSQL:', result.rows[0].now);
+
+        // Bağlantıyı serbest bırak
+        client.release();
     } catch (error) {
-        console.error('Error retrieving PostgreSQL credentials from Key Vault:', error);
+        console.error('Error connecting to PostgreSQL:', error);
     }
 }
 
-// Test etmek için uygulamayı çalıştır
+// Test etmek için PostgreSQL bağlantısını gerçekleştir
 connectToPostgres();
 
 
--- config.js
+++ config.js
@@ -1,11 +1,16 @@
 const { SecretClient } = require("@azure/keyvault-secrets");
 const { DefaultAzureCredential } = require("@azure/identity");
 
-const vaultUrl = "https://kv-4453.vault.azure.net/";
+// Azure Key Vault URL'si
+const vaultUrl = process.env.KEYVAULTURL; // KEYVAULTURL ortam değişkeninden alınır
 
+// Kimlik doğrulama sağlayıcı
 const credential = new DefaultAzureCredential();
+
+// Key Vault istemci nesnesi oluştur
 const client = new SecretClient(vaultUrl, credential);
 
+// Key Vault'tan gizli bilgilere erişim sağlayan fonksiyon
 async function getSecret(name) {
     const secret = await client.getSecret(name);
     return secret.value;
