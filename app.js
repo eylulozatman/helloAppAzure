aa68dcd5a83588100e2836f6dc5af04304c6cdaa
eylulozatman <eylulozatman@gmail.com>
2024-03-30 18:23:07 +0300

    config changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 //DEPLOYMENT
 const app = express();
 const port = 3000; 
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('my initial function, Hello');
