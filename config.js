85c6462b49a01a11f7a8289071f67f85c7ee927e
eylulozatman <eylulozatman@gmail.com>
2024-03-30 23:04:52 +0300

    changes

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 const { SecretClient } = require("@azure/keyvault-secrets");
 const { DefaultAzureCredential } = require("@azure/identity");
 
-// Azure Key Vault URL'si
-const vaultUrl = process.env.KEYVAULTURL; // KEYVAULTURL ortam değişkeninden alınır
+const vaultUrl = process.env.KEYVAULTURL; 
 
-// Kimlik doğrulama sağlayıcı
 const credential = new DefaultAzureCredential();
 
-// Key Vault istemci nesnesi oluştur
 const client = new SecretClient(vaultUrl, credential);
 
-// Key Vault'tan gizli bilgilere erişim sağlayan fonksiyon
 async function getSecret(name) {
     const secret = await client.getSecret(name);
     return secret.value;
